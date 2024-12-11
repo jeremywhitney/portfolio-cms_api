97dad3d9b41c99c153218a9ca2570bc53c8a694e
Jeremy Whitney <bearwhitness@gmail.com>
2024-12-11 12:44:54 -0600

    Established routing and included some temporary [AllowAny] permissions for API testing in Postman

--- portfoliocmsapi/portfolio/views/tech_stack_viewset.py
+++ portfoliocmsapi/portfolio/views/tech_stack_viewset.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 from ..models import TechStack
 from ..serializers.tech_stack_serializer import TechStackSerializer
 
 
 class TechStackViewSet(viewsets.ModelViewSet):
+    permission_classes = [AllowAny]
     queryset = TechStack.objects.all()
     serializer_class = TechStackSerializer
